3e09223056ca6ecdb94780596d0e6d26e570cc84
Masatoshi Nishimura <massa_n@yahoo.ca>
2015-05-19 14:31:43 -0400

    Added update function

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -30,4 +30,24 @@ myApp.controller('AppCtrl', function($scope, $http) {
     		console.log(error);
     	});
     }
+
+    $scope.edit = function(id){
+    	console.log(id);
+    	$http.get('/contactlist/' + id).then(function(result){
+    		console.log(result);
+    		$scope.contact = result.data;	
+    	}, function(error){
+    		console.log(error);
+    	})
+    }
+
+    $scope.update = function(){
+    	console.log($scope.contact._id);
+    	$http.put('/contactlist/' + $scope.contact._id, $scope.contact).then(function(result){
+    		console.log(result)
+    		refresh();
+    	}, function(error){
+    		console.log(error);
+    	});
+    }
 })
